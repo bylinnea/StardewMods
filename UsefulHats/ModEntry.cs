65f7e2f5fa93bf92cec4480e4b57a21ccc9c2198
bylinnea <43776557+bylinnea@users.noreply.github.com>
2026-07-15 22:55:57 +0200

    Add Useful Hats mod skeleton

---
+++ UsefulHats/ModEntry.cs
@@ -0,0 +1,12 @@
+﻿using StardewModdingAPI;
+
+namespace UsefulHats
+{
+    public class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            this.Monitor.Log("Useful Hats loaded!", LogLevel.Debug);
+        }
+    }
+}
