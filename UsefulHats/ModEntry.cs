46386ca0cb9df111d0f5bad5bb7a6d859fadcbe5
bylinnea <43776557+bylinnea@users.noreply.github.com>
2026-07-16 23:42:32 +0200

    feat: apply +1 Fishing buff while the Blue Ribbon hat is worn

--- UsefulHats/ModEntry.cs
+++ UsefulHats/ModEntry.cs
@@ -1,12 +1,79 @@
 ﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Objects;
+using StardewValley.Buffs;
 
 namespace UsefulHats
 {
     public class ModEntry : Mod
     {
+        private const SButton LogHatKey = SButton.F5;
+
+        private const string HatBuffId = "byLinnea.UsefulHats_HatBuff";
+
+                private const string BlueRibbonId = "BlueRibbon";
+
         public override void Entry(IModHelper helper)
         {
             this.Monitor.Log("Useful Hats loaded!", LogLevel.Debug);
+
+            helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+
+            helper.Events.GameLoop.UpdateTicked += this.OnUpdateTicked;
+        }
+
+        private void OnUpdateTicked(object? sender, UpdateTickedEventArgs e)
+        {
+            if (!Context.IsWorldReady)
+                return;
+
+            if (!e.IsMultipleOf(15))
+                return;
+
+            string? wornHatId = Game1.player.hat.Value?.ItemId;
+            bool shouldHaveBuff = wornHatId == BlueRibbonId;
+            bool hasBuff = Game1.player.hasBuff(HatBuffId);
+            if (shouldHaveBuff && !hasBuff)
+            {
+                Buff buff = new Buff(
+                    id: HatBuffId,
+                    displayName: "Blue Ribbon",
+                    duration: Buff.ENDLESS,
+                    effects: new BuffEffects
+                    {
+                        FishingLevel = { 1 }
+                    });
+
+                Game1.player.applyBuff(buff);
+                this.Monitor.Log("Applied Fishing Blue Ribbon (+1 Fishing).", LogLevel.Debug);
+            }
+            else if (!shouldHaveBuff && hasBuff)
+            {
+                Game1.player.buffs.Remove(HatBuffId);
+                this.Monitor.Log("Removed Blue Ribbon buff.", LogLevel.Debug);
+            }
+        }
+        private void OnButtonPressed(object? sender, ButtonPressedEventArgs e)
+        {
+            if (e.Button != LogHatKey)
+                return;
+
+            if (!Context.IsWorldReady)
+                return;
+
+            Hat? hat = Game1.player.hat.Value;
+            if (hat is null)
+            {
+                this.Monitor.Log("No hat is currently worn.", LogLevel.Info);
+                return;
+            }
+
+            this.Monitor.Log(
+                $"Worn hat: {hat.DisplayName} " +
+                $"| ItemId={hat.ItemId} " +
+                $"| QualifiedItemId={hat.QualifiedItemId}",
+                LogLevel.Info);
         }
     }
 }
